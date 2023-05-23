6a3b3d97b20009ffacba6f062d0a9f77d828cbc6
eysaif <u4saif@gmail.com>
2023-05-24 02:15:02 +0530

    formating done and errorhandling

--- simplyhire-api/server.js
+++ simplyhire-api/server.js
@@ -3,18 +3,36 @@ const colors = require("../simplyhire-api/utilities/consolColors");
 const dotenv = require("dotenv");
 const connectDB = require("../simplyhire-api/configs/dbConnection");
 const app = express();
-const {generateToken, verifyToken} = require("./utilities/authToken");
-const {authMiddleware} = require("../simplyhire-api/middleware/jwtmiddleware");
+const { generateToken, verifyToken } = require("./utilities/authToken");
+const {
+  authMiddleware,
+} = require("../simplyhire-api/middleware/jwtmiddleware");
 const PORT = process.env.PORT || 5000;
-const authRoutes = require("../simplyhire-api/routes/authRoute")
-//*******Configure ENV Path**********//
-dotenv.config({path:'./configs/config.env'})
+const authRoutes = require("../simplyhire-api/routes/authRoute");
+const {
+  errorHandler,
+} = require("../simplyhire-api/middleware/errorMiddleware");
 
-//*******Database Conection**********//
+/**
+ * Configure ENV Path
+ */
+dotenv.config({ path: "./configs/config.env" });
+
+/**
+ * Database Conection
+ */
 connectDB();
 app.use(express.json());
 
-app.use("/api/v1/auth",authRoutes);
+/**
+ * App Routes
+ */
+app.use("/api/v1/auth", authRoutes);
+
+/**
+ * Use the ErrorHandler to catch app wise Errors
+ */
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(
