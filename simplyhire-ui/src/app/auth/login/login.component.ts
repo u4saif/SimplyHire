a525e8104200cf30fbf5e4fce3f7984d49a3772b
eysaif <u4saif@gmail.com>
2023-05-30 23:21:06 +0530

    registeration form validation added

--- simplyhire-ui/src/app/auth/login/login.component.ts
+++ simplyhire-ui/src/app/auth/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   passwordVisible = false;
   password?: string;
   isLoginFormVisible: boolean = true;
-  isFormLoading:boolean = false;
+  isFormLoading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -24,21 +24,26 @@ export class LoginComponent implements OnInit {
     private StorageService: StorageService
   ) {}
   loginForm: FormGroup = this.fb.group({
-    username: ['', [Validators.required,Validators.email]],
+    username: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
     rememberUser: false,
   });
 
   registerForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
-    username: ['', [Validators.required,Validators.email]],
-    password: ['', [Validators.required,Validators.minLength(8)]],
-    confirmPassword: ['', [Validators.required,Validators.minLength(8)]],
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: [
+      '',
+      {
+        validators: [Validators.required, Validators.minLength(8)],
+      },
+    ],
   });
 
   ngOnInit(): void {
     //if (this.StorageService.getToken('authToken'))
-      //this.route.navigate(['dashboard/home']);
+    //this.route.navigate(['dashboard/home']);
   }
 
   toggleForm() {
@@ -50,17 +55,17 @@ export class LoginComponent implements OnInit {
    */
   login() {
     const { username, password } = this.loginForm.value;
-    this.isFormLoading=true;
+    this.isFormLoading = true;
     this.auth.userLogin('auth/login', { username, password }).subscribe(
       (response: any) => {
-        this.isFormLoading=false;
+        this.isFormLoading = false;
         this.StorageService.setToken('authToken', response['token']);
         this.StorageService.setToken('refToken', response['token']);
         this.message.create('success', `Logged In successfully`);
         this.route.navigate(['dashboard/home']);
       },
       (error) => {
-        this.isFormLoading=false;
+        this.isFormLoading = false;
         this.message.create(
           'error',
           `Unable to login. ${error?.error?.error || error?.error?.message}`
@@ -70,32 +75,34 @@ export class LoginComponent implements OnInit {
     );
   }
 
-/**
- * Register a new User, login & store the token
- */
+  /**
+   * Register a new User, login & store the token
+   */
   register() {
     const { name, username, password } = this.registerForm.value;
-    this.auth.userLogin('auth/register', { name, username, password }).subscribe(
-      (resp: any) => {
-        this.StorageService.setToken('authToken', resp['token']);
-        this.StorageService.setToken('refToken', resp['token']);
-        this.message.create('success', `Logged In successfully`);
-        this.route.navigate(['dashboard/home']);
-      },
-      (error) => {
-        this.message.create(
-          'error',
-          `Unable to login. ${error?.error?.error || error?.error?.message}`
-        );
-      }
-    );
+    this.auth
+      .userLogin('auth/register', { name, username, password })
+      .subscribe(
+        (resp: any) => {
+          this.StorageService.setToken('authToken', resp['token']);
+          this.StorageService.setToken('refToken', resp['token']);
+          this.message.create('success', `Logged In successfully`);
+          this.route.navigate(['dashboard/home']);
+        },
+        (error) => {
+          this.message.create(
+            'error',
+            `Unable to login. ${error?.error?.error || error?.error?.message}`
+          );
+        }
+      );
   }
 
-  get username(){
-    return this.loginForm.controls['username']
+  get username() {
+    return this.loginForm.controls['username'];
   }
 
-  get username2(){
-    return this.registerForm.controls['username']
+  get registerUsername() {
+    return this.registerForm.controls['username'];
   }
 }
