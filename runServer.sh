b8f78640a96896b69d67bf301e15c74c62a133ff
eysaif <u4saif@gmail.com>
2023-06-01 12:47:24 +0530

    css changes

---
+++ runServer.sh
@@ -0,0 +1,5 @@
+echo "Running Backend Server at port 5000"
+cd simplyhire-api/
+npm run devstart
+echo "Server is Up !!!!"
+$SHELL
---
+++ runUI.sh
@@ -0,0 +1,5 @@
+echo "Running Backend Server at port 5000"
+cd simplyhire-ui/
+npm start
+echo "Frontend is Up !!!!"
+$SHELL
