29c25623d00f761b410c10cd35b37f131e3141a9
eysaif <u4saif@gmail.com>
2023-06-07 19:45:55 +0530

    database corrected

--- simplyhire-api/configs/dbConnection.js
+++ simplyhire-api/configs/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async ()=>{
     try{
-        const connectionStatus = await mongoose.connect(process.env.MONGO_URL)
+        const connectionStatus = await mongoose.connect(process.env.MONGO_URL, {dbName:"simplyHire"})
         console.log(`DataBase Connected Successfully. 🚨PORT:${connectionStatus.connection.port}`.warn);
     }catch(error){
         console.log(`Unable to connect DB${error}`.error);
